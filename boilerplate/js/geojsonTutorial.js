c6e6a0f579e143466c007fb70cc3f0f4053f5bbc
jinskee <98485932+jinskee@users.noreply.github.com>
2022-02-17 20:41:35 -0600

    Working on Tutorials

---
+++ boilerplate/js/geojsonTutorial.js
@@ -0,0 +1,197 @@
+var map = L.map('map',{
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map)
+
+//An Example of a a simple GeoJSON feature:
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}
+    
+
+
+//GeoJSON objects are added to the map through a GeoJSON layer. 
+//To create it and add it to the map we can use the following code:
+L.geoJSON(geojsonFeature).addTo(map);
+
+//Messing around adding a geojson feature
+/*var bridgerFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Bridger Bowl",
+        "amenity": "Ski Area",
+        "popupContent": "Ski the Cold Smoke"
+    },
+    "geometry": {
+        "type": "point",
+        "coordinates": [-110.898194, 45.817678]
+    }
+};
+
+//Add feature to map
+L.geoJSON(bridgerFeature).addTo(map);*/
+
+//GeoJSON objects may also be passed as an array of valid GeoJSON objects.
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+//Alternatively, we could create an empty GeoJSon layer and assign it to a variable so that we can add
+//more features to it later
+var myLayer = L.geoJSON().addTo(map);
+myLayer.addData(geojsonFeature);
+
+//The style option can be used to style features two different
+//ways. First we can pass a simple object that styles all paths (polylines 
+//and polygons) the same way:
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+//Alternatively, we can pass a funciton that styles individual features based on their properties.
+//In the example below we check the "party" property and style our 
+//polygons accordingly
+
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
+//Points are Handled differently than polylines and polygons. 
+//By default simple markers are drawn for GeoJson points.
+//We can alter this by passing a pointToLayer funciton in a GeoJson options
+//object when creating the GeoJson layer.
+//This funciton is passed a latlng and should return an instance of ILayer in 
+//this case likely a marker or circlemarker
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+L.geoJSON(someGeojsonFeature, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
+
+//The onEachFearture option is a function that gets called on each feature before adding it to a GeoJson layer. A common reason to use this option is to attach a popup to features when they are clicked.
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var someFeatures = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "show_on_map": true
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": false
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.98404, 39.74621]
+    }
+}];
+
+L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    }
+}).addTo(map);
--- boilerplate/js/quickstartTutorial.js
+++ boilerplate/js/quickstartTutorial.js
@@ -1,6 +1,10 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-var Stadia_Outdoors = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+/*var Stadia_Outdoors = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map)*/
+var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map)
